07813f1410d67f681250fa1bfd546d4335b7af11
zhutai <zhutai@outlook.com>
2018-04-16 17:57:01 +0800

    test：文件提交测试

--- src/components/csgNav.vue
+++ src/components/csgNav.vue
@@ -13,7 +13,7 @@
     <template slot="title">我的工作台</template>
     <el-menu-item index="2-1">选项1</el-menu-item>
     <el-menu-item index="2-2">选项2</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
+    <el-menu-item index="2-3">选项34</el-menu-item>
     <el-submenu index="2-4">
       <template slot="title">选项4</template>
       <el-menu-item index="2-4-1">选项1</el-menu-item>
